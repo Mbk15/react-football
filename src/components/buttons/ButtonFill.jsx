d14e3b67b2e046a10787077365c83da261fe66ec
Mbk15 <mubarakmuhammadjamiu@gmail.com>
2022-06-08 02:56:19 +0100

    Created Component Structure and Theme setup for color and typography

---
+++ src/components/buttons/ButtonFill.jsx
@@ -0,0 +1,14 @@
+const ButtonFill = () => {
+  return (
+    <>
+      <button
+        type="button"
+        className="focus:outline-none text-white bg-primary hover:bg-secondary focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-8 py-3 mr-2 mb-2 dark:focus:ring-yellow-900"
+      >
+        Yellow
+      </button>
+    </>
+  );
+};
+
+export default ButtonFill;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FEA013",
+        primary_red: "#D23131",
+        secondary: "#BA4A0C",
+        neutral_primary: "#222222",
+        neutral_secondary: "#2D2D2D",
+      },
+    },
   },
   plugins: [],
 };
